b8c45af1c3c127c4ceccc76c71aa37fc5e4f3206
Aatish Gupta <156168433+aatishgupta25@users.noreply.github.com>
2025-07-19 13:08:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,6 @@ export default function Home() {
         <h1 className="text-3xl font-bold mb-4 text-gray-800">
           Sign in to continue!
         </h1>
-        <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-          Sign in with GitHub
-        </button>
       </div>
     </div>
   );
